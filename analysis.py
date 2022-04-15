cf3cbe81e48ac82143d2cdf9360f6f18b0741463
Ilovecheesecake <hafogi9895@matmayer.com>
2022-04-15 19:36:59 +0100

    Improved error handling logic

--- analysis.py
+++ analysis.py
@@ -87,3 +87,4 @@ Temporary commit content for 2022-02-07T19:36:59
 Temporary commit content for 2022-03-03T19:36:59
 Temporary commit content for 2022-03-17T19:36:59
 Temporary commit content for 2022-03-27T19:36:59
+Temporary commit content for 2022-04-15T19:36:59
