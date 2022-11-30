4a7097f665b26bd10f9a9a32c4d223e33e89b990
Ilovecheesecake <hafogi9895@matmayer.com>
2022-11-30 19:36:59 +0100

    Fixed typos in documentation

--- analysis.py
+++ analysis.py
@@ -109,3 +109,4 @@ Temporary commit content for 2022-09-16T19:36:59
 Temporary commit content for 2022-09-21T19:36:59
 Temporary commit content for 2022-10-13T19:36:59
 Temporary commit content for 2022-11-13T19:36:59
+Temporary commit content for 2022-11-30T19:36:59
