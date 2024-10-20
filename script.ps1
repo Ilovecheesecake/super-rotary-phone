e27bc5a4658ed5df3cadce894b2c37bf7395e47c
Ilovecheesecake <hunght1890_rRYElpDpKKGq>
2024-10-20 19:32:58 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -89,3 +89,4 @@ Temporary commit content for 2024-09-07T19:32:58
 Temporary commit content for 2024-09-21T19:32:58
 Temporary commit content for 2024-09-29T19:32:58
 Temporary commit content for 2024-10-17T19:32:58
+Temporary commit content for 2024-10-20T19:32:58
